4513a81df4104587a7f39f56f03bb552601c760d
TUNAPRO1234 <60389660+tunapro1234@users.noreply.github.com>
2020-12-20 02:18:53 +0300

    tetsttt

--- firm/src/master.py
+++ firm/src/master.py
@@ -1,9 +1,11 @@
-from firm.src.arduino import Arduino, ArduinoTypes
+from arduino import Arduino, ArduinoTypes
+import time
+
+com_port = "COM5"
 
 # şu an için basit bir script olacak
 board = None  # global yaptığım için özür dilerim
 # ama şu an için python -i ile kullanabilmem gerekiyor
-
 """
 Bu dosya /etc/rc.local dosyası içinden çalıştırılacak ve loop içinden belirlediğimiz bir dosyayı okuyarak
 dosya üzerinde yazan değerleri arduinoya iletecek
@@ -17,7 +19,16 @@ dosyanın formatı konusunda biraz düşünmem gerekiyor.
 
 def main():
     global board
-    board = Arduino(list(range(2, 10)), "COM7", ArduinoTypes.none, 0, 1)
+    board = Arduino(
+        list(range(2, 14)),
+        com_port,
+        ArduinoTypes.none,
+        invert=1,
+        start_state=1,
+    )
+
+    # write_all denerdim ama uykum geldi
+    # anacondadan readline olayını kaldırmam lazım :-(
 
 
 if __name__ == "__main__":
